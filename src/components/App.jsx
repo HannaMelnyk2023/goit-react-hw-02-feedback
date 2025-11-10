cd422eafcf49318eae7aad0845bcf9b04f2b1e60
HannaMelnyk2023 <151830548+HannaMelnyk2023@users.noreply.github.com>
2025-11-10 19:00:40 +0200

    step 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import './App.css';
 
 export class App extends Component {
   state = {
@@ -6,44 +7,52 @@ export class App extends Component {
     neutral: 0,
     bad: 0,
   };
-  handleFeedback = (type) => {
-    this.setState((prevState) => ({
+  handleFeedback = type => {
+    this.setState(prevState => ({
       [type]: prevState[type] + 1,
     }));
   };
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+  countPositiveFeedbackPercentage() {
+    const total = this.countTotalFeedback();
+    if (total === 0) {
+      return 0;
+    }
+    const percentage = (this.state.good / total) * 100;
+    return Math.round(percentage);
+  }
   render() {
     const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+
     return (
-      <div style={{ textAligh: "center", marginTop: "40px" }} >
+      <div>
         <h1>Expresso Feedback</h1>
-        <div>
-          <button onClick={() => this.handleFeedback("good")}>Goooood</button>
-          <button onClick={() => this.handleFeedback("neutral")}>Neutral</button>
-          <button onClick={() => this.handleFeedback("bad")}>Bad</button>
+        <div className='buttons'>
+          <button onClick={() => this.handleFeedback('good')}>Goooood</button>
+          <button onClick={() => this.handleFeedback('neutral')}>
+            Neutral
+          </button>
+          <button onClick={() => this.handleFeedback('bad')}>Bad</button>
         </div>
         <h2>Statistics</h2>
-        <p>Goooood:{good}</p>
-        <p>Neutral:{neutral}</p>
-        <p>Bad:{bad}</p>
-      </div >
+        {total > 0 ? (
+          <>
+            <p>Goooood:{good}</p>
+            <p>Neutral:{neutral}</p>
+            <p>Bad:{bad}</p>
+            <p>Total: {total}</p>
+            <p> Positive feedback: {positivePercentage}%</p>
+          </>
+        ) : (
+          <p>No feedback given</p>
+        )}
+      </div>
     );
   }
 }
 
-// export const App = () => {
-//   return (
-    // <div
-    //   style={{
-    //     height: '100vh',
-    //     display: 'flex',
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     fontSize: 40,
-    //     color: '#010101'
-    //   }}
-    // >
-    //   React homework template
-    // </div>
-
-//   );
-// };
